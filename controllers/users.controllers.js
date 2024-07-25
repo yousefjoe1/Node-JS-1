a50df0a501fd3d32c2d4b3db12c6b6b87c3d20f0
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-07-25 15:56:22 +0300

    add log in function and bcrypt js

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var bcrypt = require('bcryptjs');
 
 var app = express()
 
@@ -12,25 +13,59 @@ app.use(bodyParser.json())
 const Users = require('../models/user.model')
 
 const addUser  = async (req,res)=> {
-    const newUser = new Users({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    })
-    try {
-        await newUser.save();
-        res.status(201).send(newUser); // Created (201) status code
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Error saving newUser');
-      }
+
+    const oldUser = await Users.findOne({email:req.body.email})
+
+    if(oldUser){
+        res.status(400).send({status:'error',data: null,code: 400,msg:'Email exist .. choose another one.'})
+    }else {
+        const {username,email,password} = req.body;
+
+        // password hashing
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        const newUser = new Users({
+            username: username,
+            email: email,
+            password: hashedPassword,
+        })
+        try {
+            await newUser.save();
+            res.json({status:'success',data: newUser,code: 201,msg:'User Created'})
+          } catch (error) {
+            console.error(error);
+            res.json({status:'error',data: null,code: 400,msg:'Error in registeration'})
+          }
+    }
+
+
 }
 
 const getUsers  = async (req,res)=> {
-    const users = await Users.find()
+    const users = await Users.find({},{'__v':false,'password': false})
     res.json({status:'success',data: users})
 }
 
+const loginUser = async(req,res)=> {
+    const {email,password} = req.body;
+    if(!email && !password){
+        res.status(400).send({status:'error',data: null,code: 400,msg:'Fill the form first'})
+    }
+
+    const user = await Users.findOne({email:email})
+    if(!user){
+        return res.status(400).send({status:'error',data: null,code: 400,msg:'Wrong details'})
+    }
+
+    const matchedPassword = await bcrypt.compare(password,user.password)
+
+    if(user && matchedPassword){
+        return res.status(200).send({status:'success',data: null,code: 200,msg:'Loged in'})
+    }else{
+        return res.status(400).send({status:'error',data: null,code: 400,msg:'wrong password or email'})
+
+    }
+}
 
 const getUser = async (req,res)=> {
     const user = await Users.findById(req.params.userId)
@@ -43,5 +78,5 @@ const getUser = async (req,res)=> {
 module.exports = {
     getUsers,
     getUser,
-    addUser
+    addUser,loginUser
 }
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,4 +1,4 @@
-const  {getUsers, addUser}  = require('../controllers/users.controllers')
+const  {getUsers, addUser, loginUser}  = require('../controllers/users.controllers')
 const exp = require('express')
 
 const router = exp.Router()
@@ -6,5 +6,6 @@ const router = exp.Router()
 router.get('/',getUsers)
 
 router.post('/register',addUser)
+router.post('/login',loginUser)
 
 module.exports = router
