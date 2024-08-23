ad36eddaab3d541507e517c108139b1f446059d4
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-08-23 09:45:07 +0300

    up pr con * route

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 // parse application/json
 app.use(bodyParser.json())
 
-app.use('/api/uploads',express.static(path.join(__dirname,'api/uploads')))
+app.use('/imgs/uploads',express.static(path.join(__dirname,'imgs/uploads')))
 
 const url = process.env.USER_ID;
 
--- routes/products.route.js
+++ routes/products.route.js
@@ -7,7 +7,7 @@ const exp = require('express')
 const multer  = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'api/uploads')
+      cb(null, 'imgs/uploads')
     },
     filename: function (req, file, cb) {
         const ext = file.mimetype.split('/')[1]
