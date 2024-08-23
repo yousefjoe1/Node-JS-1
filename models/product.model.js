0e725c7e044cf3366c13252c753ab84eadad6345
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-08-23 09:58:19 +0300

    up

--- models/product.model.js
+++ models/product.model.js
@@ -26,6 +26,7 @@ const productSchema = new mongoose.Schema({
     },
     image: {
       type: String, // Store image URL (consider using cloud storage for efficiency)
+      default: '/defaultimg.jpg'
       // Or:
       // type: Buffer, // Store image data as binary (less common)
     },
